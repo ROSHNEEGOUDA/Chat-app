4cd279686a883e8267a9a25de13c199dc17c6077
ROSHNEEGOUDA <roshnee111103@gmail.com>
2025-02-28 17:09:06 +0530

    add the vercel.json

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  base: "/", // ✅ Fix for Vercel to load assets properly
+  build: {
+    outDir: "dist",
+  },
 })
