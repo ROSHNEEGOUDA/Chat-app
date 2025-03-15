fc4f61fd7df4afe9cc3a898e89f5dd99a7758f97
ROSHNEEGOUDA <roshnee111103@gmail.com>
2025-03-15 17:30:21 +0530

    fix bug

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ const io = socketIo(server, {
 });
 
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5500"],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
